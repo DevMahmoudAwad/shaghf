3a061a81b9af79cc7addce3438d782b3136c10a3
DevMahmoudAwad <developer.man22@gmail.com>
2024-09-30 00:27:04 +0300

    "Commented out Dio imports and related code in multiple files"

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shaghf/features/authentication_screen/presentation/views/login_page.dart';
+import 'package:shaghf/features/splash_screen/presentation/view/splashScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomePage(),
+      home: Splashscreen(),
     );
   }
 }
