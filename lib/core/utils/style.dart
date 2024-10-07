b911572b85e9a49c2521e63cff6b42d1b39d3f34
Nadamohamed188 <hamadanada257@gmail.com>
2024-10-07 12:57:21 +0300

    update3

--- lib/core/utils/style.dart
+++ lib/core/utils/style.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
 abstract class Styles {
-<<<<<<< HEAD
-  static const textStyle16 = TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeight.w400,
-  );
-=======
->>>>>>> c6de7906574a32cba71f4c10167e076ba676a381
   static const textStyle14 = TextStyle(
       fontSize: 14, fontWeight: FontWeight.normal, color: Color(0xffffffff));
 
@@ -22,15 +15,6 @@ abstract class Styles {
   static const textStyle12 = TextStyle(
       fontSize: 12, fontWeight: FontWeight.normal, color: Color(0xffffffff));
   static const textStyle17 = TextStyle(
-<<<<<<< HEAD
-    fontSize: 17,
-    fontWeight: FontWeight.w400,
-  );
-  static const textStyle20 = TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.w600,
-  );
-=======
       fontSize: 17, fontWeight: FontWeight.normal, color: Color(0xffffffff));
   static const textStyle18 = TextStyle(
       fontSize: 18, fontWeight: FontWeight.normal, color: Color(0xffffffff));
@@ -38,5 +22,4 @@ abstract class Styles {
       fontSize: 20, fontWeight: FontWeight.normal, color: Color(0xffffffff));
   static const textStyle32 = TextStyle(
       fontSize: 32, fontWeight: FontWeight.normal, color: Color(0xffffffff));
->>>>>>> c6de7906574a32cba71f4c10167e076ba676a381
 }
