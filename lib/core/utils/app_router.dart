1772c8db660a76c84de829659a495a1405a3917f
Nadamohamed188 <hamadanada257@gmail.com>
2024-10-07 12:32:46 +0300

    "Removed textStyle16, textStyle20, and textStyle17 (with different properties) and updated textStyle14, textStyle17, textStyle18, and other text styles to have consistent font weight and color."

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -3,15 +3,10 @@ import 'package:shaghf/features/authentication_screen/presentation/views/forgotp
 import 'package:shaghf/features/authentication_screen/presentation/views/login_page.dart';
 import 'package:shaghf/features/authentication_screen/presentation/views/signup_page.dart';
 import 'package:shaghf/features/authentication_screen/presentation/views/verification_page.dart';
-<<<<<<< HEAD
-import 'package:shaghf/features/rooms_screen/presentation/view/bottom_cheet/details_rooms_body.dart';
-import 'package:shaghf/features/rooms_screen/presentation/view/bottom_cheetView.dart';
-=======
 import 'package:shaghf/features/home_screen/presentation/views/home_page.dart';
 import 'package:shaghf/features/home_screen/presentation/views/widgets/home_widgets/drawer.dart';
 import 'package:shaghf/features/rooms_screen/presentation/view/bottom_cheetView.dart';
 import 'package:shaghf/features/rooms_screen/presentation/view/room_page.dart';
->>>>>>> c6de7906574a32cba71f4c10167e076ba676a381
 import 'package:shaghf/features/splash_screen/presentation/view/OnboardingView.dart';
 import 'package:shaghf/features/splash_screen/presentation/view/splashScreen.dart';
 
@@ -21,14 +16,6 @@ abstract class AppRouter {
   static const KSignUpPage = "/signup_page";
   static const KForgotPasswordPage = "/forgotpassword_page";
   static const KVerificationPage = "/verification_page";
-<<<<<<< HEAD
-  static const KHomePage = "/home_page";
-  static const RoomsPath = "/RoomPage";
-  static const KRoomsDetails = "/BottomCheetView";
-  static final router = GoRouter(routes: [
-    GoRoute(path: '/', builder: (context, state) => Splashscreen()),
-    GoRoute(path: KLoginPage, builder: (context, state) => LoginPage()),
-=======
   static const KHomePage = "/HomePage";
   static const RoomsPath = "/RoomPage";
   static const KRoomsDetails = "/BottomCheetView";
@@ -37,7 +24,6 @@ abstract class AppRouter {
     GoRoute(path: '/', builder: (context, state) => Splashscreen()),
     GoRoute(path: KLoginPage, builder: (context, state) => LoginPage()),
     GoRoute(path: KHomePage, builder: (context, state) => HomePage()),
->>>>>>> c6de7906574a32cba71f4c10167e076ba676a381
     GoRoute(
         path: KOnboardingView, builder: (context, state) => OnboardingView()),
     GoRoute(
@@ -54,13 +40,6 @@ abstract class AppRouter {
     ),
     GoRoute(
       path: RoomsPath,
-<<<<<<< HEAD
-      builder: (context, state) => const RoomPage(),
-    ),
-    GoRoute(
-      path: KRoomsDetails,
-      builder: (context, state) => const BottomCheetView(),
-=======
       builder: (context, state) => RoomPage(),
     ),
     GoRoute(
@@ -70,7 +49,6 @@ abstract class AppRouter {
     GoRoute(
       path: KHomeDrawerPage,
       builder: (context, state) => MyDrawer(),
->>>>>>> c6de7906574a32cba71f4c10167e076ba676a381
     )
   ]);
 }
